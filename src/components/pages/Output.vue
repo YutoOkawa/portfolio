b58b5d59921ec9004d609c888b49126d73143de8
YutoOkawa <is0344rf@ed.ritsumei.ac.jp>
2020-06-04 23:05:34 +0900

    Outputリストの充実

--- src/components/pages/Output.vue
+++ src/components/pages/Output.vue
@@ -2,8 +2,10 @@
     <div class="output">
         <h1 class="content-title"> {{ msg }} </h1>
         <div class="content" v-for="(output, index) in outputs" :key="index">
-            <h3> {{ output.name }} </h3>
+            <a :href="output.url">{{ output.name }}</a>
             <p> {{ output.text }} </p>
+            <p>使用言語</p>
+            <p>{{ output.lang }}</p>
         </div>
     </div>
 </template>
@@ -16,8 +18,22 @@ export default {
             msg: 'Output List',
             outputs: [
                 {
-                    name: 'Test',
-                    text: 'Test'
+                    name: 'Attribute-Based Signature',
+                    text: '属性ベース署名のアルゴリズムをPythonで実装し、Node.jsから呼び出すサンプルプログラム',
+                    url: 'https://github.com/YutoOkawa/node_ABS',
+                    lang: 'Python, Node.js'
+                },
+                {
+                    name: 'My Portfolio Site',
+                    text: '初めてのフロントエンド作成のため、様々なサイトを参考にし作成しました',
+                    url: 'https://yutookawa.github.io/portfolio',
+                    lang: 'Vue.js'
+                },
+                {
+                    name: 'partDownloader',
+                    text: 'メルカリの「プログラミング言語Go完全入門」にて取り組んだ課題で作成したプログラムです。goroutineを用いて、Web上のファイルを高速に取得することができます。',
+                    url: 'https://github.com/YutoOkawa/partDownloader',
+                    lang: 'golang'
                 }
             ]
         }
@@ -33,10 +49,16 @@ div.output {
 }
 
 div.content {
-    background-color: #eee;
+    background-color: white;
+    box-shadow: 0px 0px 10px 0px;
+    border-radius: 20px 20px 20px 20px;
     display: inline-block;
     margin: 10px;
-    width: 30%;
+    width: 40%;
+}
+
+a {
+    font-size: 2em;
 }
 
 h3 {
