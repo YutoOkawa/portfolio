d866d15ea979b31a5cabc278e01149f0cfc8c3fe
YutoOkawa <is0344rf@ed.ritsumei.ac.jp>
2020-06-05 17:53:55 +0900

    Profileの実装、レイアウトの調整

--- src/components/pages/Profile.vue
+++ src/components/pages/Profile.vue
@@ -1,7 +1,21 @@
 <template>
-    <div class="profile">
-        <h1 class="content-title"> {{ msg }} </h1>
-            <p> {{ text }} </p>
+    <div>
+        <h1 class="content-title">
+            <font-awesome-icon icon="user-circle" class="icon"/> {{ msg }}
+        </h1>
+        <div class="profile">
+            <div class="content" v-for="(content, index) in contents" :key="index">
+                <h1 class="profile_title">
+                    <font-awesome-icon :icon="content.icon" class="icon"/> {{ content.title }}
+                </h1>
+                <h3 v-if="content.url!=null">
+                    <a :href="content.url"> {{ content.text }} </a>
+                </h3>
+                <h3 v-else class="profile_text">
+                    {{ content.text }}
+                </h3>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -11,25 +25,68 @@ export default {
     data () {
         return {
             msg: 'Profile',
-            text: '暗号とFIDOについて勉強しています。'
+            text: '暗号とFIDOについて勉強しています。',
+            contents: [
+                {
+                    title: 'Name',
+                    icon: 'pen-alt',
+                    text: '大川 悠人 (Ookawa Yuto)'
+                },
+                {
+                    title: 'University',
+                    icon: 'university',
+                    text: '立命館大学院 情報理工学研究科'
+                },
+                {
+                    title: 'Laboratory',
+                    icon: 'tag',
+                    text: 'サイバーセキュリティ研究室',
+                    url: 'http://www.cysec.cs.ritsumei.ac.jp/'
+                },
+                {
+                    title: 'Research Keyword',
+                    icon: 'key',
+                    text: 'FIDO, WebAuthn, 属性ベース署名'
+                },
+                {
+                    title: 'Github',
+                    icon: ['fab', 'github'],
+                    text: 'YutoOkawa',
+                    url: 'https://github.com/YutoOkawa/'
+                },
+                {
+                    title: 'Facebook',
+                    icon: ['fab', 'facebook'],
+                    text: 'Ookawa Yuto',
+                    url: 'https://www.facebook.com/ookawa.yuuto.1/'
+                }
+            ]
         }
     }
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+@import '@/assets/style/_mixin.scss';
+
 
 div.profile {
-    background-color: white;
-    height: 500px;
+    @include box-wrapper;
 }
 
-h1 {
-    color: black;
+div.content {
+    @include box(30%)
 }
 
-p {
-    width: 60%;
-    display: inline-block;
+div.profile_title {
+    @include box-title;
+}
+
+div.profile_text {
+    @include box-text;
+}
+
+.icon {
+    font-size: 1em;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import smoothScroll from 'vue-smoothscroll'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserCircle, faPenAlt, faUniversity, faTag, faKey} from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faFacebook } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faUserCircle, faPenAlt, faUniversity, faTag, faKey, faGithub, faFacebook)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 Vue.use(smoothScroll)
 Vue.config.productionTip = false
 
